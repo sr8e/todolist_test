32c15c5545dc42493b17273b95aea7f6e3ea4510
sr8e <42915825+sr8e@users.noreply.github.com>
2019-12-12 03:43:35 +0900

    improved

--- todolist/todolistapp/views.py
+++ todolist/todolistapp/views.py
@@ -6,6 +6,11 @@ import datetime
 from dateutil.relativedelta import relativedelta
 
 #
+class InvalidValueError(Exception):
+    def __init__(self,msg):
+        self.msg=msg
+
+#
 prop_keys=["id","taskname","deadline","priority","do_repeat","interval_type","interval","parent"]
 
 def checkdue(tasks, pk=-1):
@@ -51,30 +56,64 @@ def makenewtask(req):
     t_overdue,t_intime,n=checkdue(tasks)
 
     prop_values=[-1,"",None,0,False,-1,"",-1]
-    page={"title":"Create",}
+    msg=req.GET['msg'] if 'msg' in req.GET else None
+    page={"title":"Create","msg":msg}
     context={prop_keys[i]:prop_values[i] for i in range(len(prop_keys))}
     
     return render(req, 'todolistapp/newtask.html',{'t_overdue':t_overdue, 't_intime':t_intime, 'n_overdue':n, 'page':page, 'context':context})
 
 def registertask(req):
     # register task
-    tz=datetime.datetime.now().astimezone().tzinfo
+    now=datetime.datetime.now().astimezone()
+    tz=now.tzinfo
     keys=["dl_y","dl_mon","dl_d","dl_h","dl_min"]
-    pk=int(req.POST['pk'])
-    prop={
-        'taskname':req.POST['taskname'],
-        'deadline':datetime.datetime(*map(int,[req.POST[k] for k in keys]),tzinfo=tz),
-        'priority':int(req.POST['priority']),
-        'do_repeat':'repeat' in req.POST,
-        'interval_type':int(req.POST['itvltype']),
-        'interval':datetime.timedelta(days=int(req.POST['interval'])) if 'repeat' in req.POST and req.POST['itvltype']=="-1" else None,
-        'parent':Task.objects.get(pk=int(req.POST['parent'])) if req.POST['parent']!="-1" else None,
-        }
+    try:
+        pk=int(req.POST['pk'])
+        if pk!=-1 and pk<1 :raise InvalidValueError("Invalid value(1)")
         
-    if pk!=-1: Task.objects.update_or_create(pk=pk,defaults=prop)
-    else:Task(**prop).save()
+        taskname=req.POST['taskname']
+        if taskname=="":raise InvalidValueError("TaskName cannot be empty.")
+        
+        deadline=datetime.datetime(*map(int,[req.POST[k] for k in keys]),tzinfo=tz)
+        if deadline<now:raise InvalidValueError("Deadline should be after current time.")
+        
+        priority=int(req.POST['priority'])
+        if not 0<=priority<=3:raise InvalidValueError("Invalid value(2)")
 
-    return http.HttpResponseRedirect(reverse("todolistapp:list"))
+        do_repeat='repeat' in req.POST
+
+        interval_type=int(req.POST['itvltype'])
+        if not -1<=interval_type<=3:raise InvalidValueError("Invalid value(3)")
+        
+        days=0
+        if do_repeat and interval_type==-1 :
+            days=int(req.POST['interval'])
+            if days<1:raise InvalidValueError("Interval should be positive integer.")
+
+        pid=int(req.POST['parent'])
+        if pid!=-1 and not Task.objects.filter(pk=pid).exists():raise InvalidValueError("Invalid value(4)")
+
+        
+        prop={
+            'taskname': taskname,
+            'deadline': deadline,
+            'priority': priority,
+            'do_repeat':do_repeat,
+            'interval_type':interval_type,
+            'interval':datetime.timedelta(days=days) if days else None,
+            'parent':Task.objects.get(pk=pid) if pid!=-1 else None,
+            }
+            
+        if pk!=-1: Task.objects.update_or_create(pk=pk,defaults=prop)
+        else:Task(**prop).save()
+
+        return http.HttpResponseRedirect(reverse("todolistapp:list"))
+    except (KeyError,TypeError,ValueError) as e:
+        # invalid form
+        return http.HttpResponseRedirect(reverse("todolistapp:new"))
+    except InvalidValueError as e:
+        return http.HttpResponseRedirect("%s?msg=%s"%(reverse("todolistapp:new"),e.msg))
+            
 
 def deletetask(req,pk):
     # delete task
